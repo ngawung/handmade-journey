01fac11b33b1f4c18ae17509f03457e393c93297
Ferdian <ferdian.artwork@gmail.com>
2022-08-09 12:35:12 -0700

    day one

---
+++ intro/day 1/main.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+    OutputDebugStringA("Hello World");
+
+    return 0;
+}
