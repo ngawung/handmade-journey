442b18f4acebe22816618e5c54ba63436fdd6675
Ferdian <ferdian.artwork@gmail.com>
2022-08-10 13:54:03 -0700

    switch to WinMain

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <windows.h>
-#include <iostream>
 
-int main() {
+
+int CALLBACK WinMain(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nShowCmd
+) {
     MessageBox(0, "Hello Handmade hero", "Handmade Hero", MB_ICONINFORMATION | MB_OK);
     return 0;
 }
